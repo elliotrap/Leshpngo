8e944a88f0ce91b205e3173363ea77ec8fec9055
Elliot Rapp <elliotmrapp@gmail.com>
2024-01-03 16:09:26 -0600

    Added the ability to let users add their own API key for using ChatGPT and the Google Cloud Text-to-speech synthesizer.

---
+++ MindfulnessAI/API/DatabaseLogin/UserDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  UserDataManager.swift
+//  MindfulnessAI
+//
+//  Created by Elliot Rapp on 12/26/23.
+//
+
+import Foundation
---
+++ MindfulnessAI/Windows/APIKeyLogin.swift
@@ -0,0 +1,18 @@
+//
+//  APIKeyLogin.swift
+//  MindfulnessAI
+//
+//  Created by Elliot Rapp on 1/2/24.
+//
+
+import SwiftUI
+
+struct APIKeyLogin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    APIKeyLogin()
+}
